e3a57b0e21a9159676ef0e303287c24cd78c5f33
sakotiya <sakotiya2707@gmail.com>
2022-11-30 13:02:54 -0800

    Adding multiple points map

--- Simple_map/static/js/logic.js
+++ Simple_map/static/js/logic.js
@@ -1,7 +1,13 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([40.7, -94.5], 5s);
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+    radius: 100
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -14,3 +20,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
