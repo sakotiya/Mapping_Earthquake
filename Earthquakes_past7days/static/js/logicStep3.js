e3181d7cecb9d7c1880783a24dd29d62523e87c7
sakotiya <sakotiya2707@gmail.com>
2022-12-01 17:45:23 -0800

    Adding

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -48,31 +48,14 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: "#ffae42",
     color: "#000000",
-    radius: getRadius(feature.properties.mag),
+    radius: getRadius(),
     stroke: true,
     weight: 0.5
   };
 }
-function getColor(magnitude) {
-  if (magnitude > 5) {
-    return "#ea2c2c";
-  }
-  if (magnitude > 4) {
-    return "#ea822c";
-  }
-  if (magnitude > 3) {
-    return "#ee9c00";
-  }
-  if (magnitude > 2) {
-    return "#eecc00";
-  }
-  if (magnitude > 1) {
-    return "#d4ee00";
-  }
-  return "#98ee00";
-}
+
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
 function getRadius(magnitude) {
@@ -85,20 +68,17 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
-  // Creating a GeoJSON layer with the retrieved data.
+    // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
     pointToLayer: function (feature, latlng) {
       console.log(data);
       return L.circleMarker(latlng)
     },
-    style: styleInfo,
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    
-    }
-  }).addTo(map);
-
-
-
-
+    style : styleInfo
+  }
+  ).addTo(map);
 });
+
+
+
+
